f44ce5f947ecd3de33e64deb6069e673486301a0
12412561 <162313891+12412561@users.noreply.github.com>
2024-03-05 15:22:16 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 
 <?php
 
-$ip = $_SERVER['REMOTE_ADDR'];
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
 
 
 // Dış servise IP bilgilerini gönder
